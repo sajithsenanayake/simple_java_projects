98cd5c1f02d0db99f44ff355a8c85187c5777635
Sajith Senanayake <91134630+sajithsenanayake@users.noreply.github.com>
2022-07-26 06:53:47 +0530

    Update Atmmachine.java

--- Atmmachine.java
+++ Atmmachine.java
@@ -2,7 +2,7 @@
 package com.mycompany.atmmachine;
 import java.util.*;
 
-public class Atmmachine { 
+public class Atmmachine {
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
